fc7517346a574899aad7c5f1e868fdeeb370f38b
Maciej-Clanet <maciejclanet@gmail.com>
2025-11-04 15:47:36 +0000

    asfasf

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,11 +1,15 @@
 import "./Auth.css";
-
+import HeroBanner from "../../components/HeroBanner/HeroBanner"
+import AuthImage1 from "../../assets/AuthBanner_01.png"
 
 export default function Auth(){
 
     return(
-        <>
-            this is the auth page
-        </>
+        <div className="auth-layout">
+            <HeroBanner image={AuthImage1}/>
+            <div className="auth-form-column">
+                stuff
+            </div>
+        </div>
     )
 }
