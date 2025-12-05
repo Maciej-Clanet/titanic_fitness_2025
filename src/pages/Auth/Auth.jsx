e254109712a941ea29bc4f81b45efa7430642c5b
Maciej-Clanet <maciejclanet@gmail.com>
2025-12-05 10:45:11 +0000

    auth forms done

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -4,16 +4,39 @@ import AuthImage1 from "../../assets/AuthBanner_01.png"
 import { useState } from "react";
 
 import { useUser } from "../../contexts/UserContext";
+import axios from "axios";
 
 function LoginForm({ toggle }) {
 
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+
+    const {login} = useUser();
+    function loginUser(event) {
+        event.preventDefault();
+        const endpoint = "http://127.0.0.1:8001/auth/login";
+        const formData = {
+            "password": password,
+            "email": email
+        }
+
+        axios.post(endpoint, formData)
+        .then(res => {
+            login(res.data)
+        })
+        .catch(err => {
+            setError(err?.response?.data?.detail || "Error occured")
+        });
+
+
+    }
 
     return (
-        <form className="auth-form">
+        <form className="auth-form" onSubmit={loginUser}>
             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="auth-input" placeholder="Email" />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="auth-input" placeholder="Password" />
+            {error ? <p>{error}</p> : null}
             <div className="toggle-section">
                 <button type="submit" className="primary-btn">Login </button>
                 <div className="toggle-group">
@@ -25,17 +48,31 @@ function LoginForm({ toggle }) {
 }
 
 function RegisterForm({ toggle }) {
-    const { user, login } = useUser();
+    const { login } = useUser();
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
 
-    function registerUser() {
-        login({
-            "username": "goober",
-            "email": "goober@goobco.com"
+    function registerUser(event) {
+        event.preventDefault()
+
+        const endpoint = "http://127.0.0.1:8001/auth/register";
+        const formData = {
+            "username": username,
+            "password": password,
+            "email": email
+        }
+
+        axios.post(endpoint, formData)
+        .then(res => {
+            login(res.data)
         })
+        .catch(err => {
+            setError(err?.response?.data?.detail || "Error occured")
+        });
+
     }
 
     return (
@@ -43,7 +80,7 @@ function RegisterForm({ toggle }) {
             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="auth-input" placeholder="Email" />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="auth-input" placeholder="Password" />
             <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="auth-input" placeholder="Display Name" />
-
+            {error ? <p>{error}</p> : null}
             <div className="toggle-section">
                 <button type="submit" className="primary-btn">Register Now</button>
                 <div className="toggle-group">
