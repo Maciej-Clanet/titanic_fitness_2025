f17277db03cc190c45c96581b55af2864f65e4c2
Maciej-Clanet <maciejclanet@gmail.com>
2025-12-09 10:14:37 +0000

    protect route

--- src/routing.jsx
+++ src/routing.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Home from "./pages/Home/Home";
 import Workouts from "./pages/Workouts/Workouts";
 import Auth from "./pages/Auth/Auth";
 import ArticlePage from "./pages/ArticlePage/ArticlePage";
+import { useUser } from "./contexts/UserContext";
 
 export default function Routing() {
+    const {user} = useUser();
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/workouts" element={<Workouts />} />
+            <Route path="/workouts" element={user ? <Workouts /> : <Navigate to="/auth" replace />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/article/:slug" element={<ArticlePage />} />
         </Routes>
