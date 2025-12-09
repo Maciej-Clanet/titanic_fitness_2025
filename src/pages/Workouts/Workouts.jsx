eb0497d7eb5bd1615fd8ae3ae57642333a8c24d9
Maciej-Clanet <maciejclanet@gmail.com>
2025-12-09 10:58:10 +0000

    workouts start

--- src/pages/Workouts/Workouts.jsx
+++ src/pages/Workouts/Workouts.jsx
@@ -1,11 +1,40 @@
 import "./Workouts.css";
-
+import {useState, useEffect} from "react";
+import { useUser } from "../../contexts/UserContext";
 
 export default function Workouts(){
 
+    // values needed for the form
+    const {user} = useUser();
+    const [kg, setKg] = useState(50);
+    const [reps, setReps] = useState(5);
+    const [selectedExercise, setSelectedExercise] = useState();
+    const [date, setDate] = useState();
+
+    // needed for filter
+    const [filter, setFilter] = useState();
+    const [loadedExercises, setLoadedExercises] = useState(["fake exercise", "second"]);
+
+    const exerciseOptions = loadedExercises.map(ex => <option value={ex}>{ex}</option>)
+
+
     return(
-        <>
+        <div className="workouts-page">
             this is the workouts page
-        </>
+            {/* filter will go here */}
+            <select 
+                className="exercise-dropdown" 
+                required 
+                onChange={(e) => setSelectedExercise(e.target.value)} 
+                value={selectedExercise}>
+                {exerciseOptions}
+            </select>
+            {selectedExercise}
+            <input type="date" requried onChange={(e) => setDate(e.target.value)} value={date} />
+            {date}
+            {/* qty selector for kg */}
+            {/* qty selector for reps */}
+            <button type="submit" className="primary-btn">ADD</button>
+        </div>
     )
 }
